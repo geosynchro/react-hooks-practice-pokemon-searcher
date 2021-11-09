e6a3d5f2d177291b0b207ef691bc0662251e5650
Nicholas Fagner <nicholasfagner@gmail.com>
2021-11-09 16:46:56 -0700

    Clickable cards complete

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({pokemon: {name, hp, sprites}}) {
+
+  //const {name, hp, sprites} = pokemon 
+
+  const [showFront, setShowFront] = useState(true)
+
+  const handleClick = () =>{
+    setShowFront((prevShowFront) => !prevShowFront)
+  }
+
   return (
     <Card>
-      <div>
-        <div className="image">
-          <img alt="oh no!" />
+      <div onClick={handleClick}>
+        <div  className="image">
+          <img src={showFront ? sprites.front: sprites.back} alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
